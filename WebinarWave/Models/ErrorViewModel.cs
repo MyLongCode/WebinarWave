5b88d338e9ad97308e4583e71263764d69afc3e9
mylongcode <mylongcode@gmail.com>
2024-06-15 18:07:04 +0500

    Add project files.

---
+++ WebinarWave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebinarWave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
