c5a202dbf8c1a349be2f39bec4989208b3218d3e
mylongcode <mylongcode@gmail.com>
2024-06-24 21:56:09 +0500

    add function insert new rooms

--- WebinarWave/Controllers/ChatController.cs
+++ WebinarWave/Controllers/ChatController.cs
@@ -18,5 +18,32 @@ namespace WebinarWave.Controllers
             var messages = db.Messages.Include(u => u.User).ToList();
             return View(messages);
         }
+
+        [Route("/room/create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("/room/create")]
+        [HttpPost]
+        public IActionResult Create(string roomName)
+        {
+            var room = new Models.Room
+            {
+                Name = roomName,
+            };
+            db.Rooms.Add(room);
+            db.SaveChanges();
+            return RedirectToAction("JoinRoom", room.Id);
+
+        }
+        [Route("/room/{roomId}")]
+        [HttpGet]
+        public IActionResult JoinRoom(int roomId)
+        {
+            var messages = db.Messages.Include(u => u.User).Select(m => m.RoomId == roomId).ToList();
+            return View(messages);
+        }
     }
 }
